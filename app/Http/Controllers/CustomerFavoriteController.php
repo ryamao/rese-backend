1058d1c2878d81bc5402435c6b06926dab8499a2
ryamao <yamao.ryouta@gmail.com>
2024-03-03 12:58:19 +0900

    お気に入り追加APIのスキーマを定義する

---
+++ app/Http/Controllers/CustomerFavoriteController.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Models\Shop;
+use App\Models\User;
+use Illuminate\Http\Response;
+use OpenApi\Attributes as OA;
+
+class CustomerFavoriteController extends Controller
+{
+    #[OA\Post(
+        operationId: 'post-customer-shop-favorite',
+        path: '/customers/{user}/shops/{shop}/favorite',
+        tags: ['Favorite'],
+        summary: 'お気に入り登録',
+        description: 'ユーザー(一般会員)が飲食店をお気に入り登録する',
+    )]
+    #[OA\PathParameter(ref: '#/components/parameters/user-id')]
+    #[OA\PathParameter(ref: '#/components/parameters/shop-id')]
+    #[OA\Response(
+        response: 201,
+        ref: '#/components/responses/created',
+    )]
+    #[OA\Response(
+        response: 401,
+        ref: '#/components/responses/unauthorized',
+    )]
+    #[OA\Response(
+        response: 403,
+        ref: '#/components/responses/forbidden',
+    )]
+    #[OA\Response(
+        response: 404,
+        ref: '#/components/responses/not-found',
+    )]
+    public function store(User $user, Shop $shop): Response
+    {
+        return response()->noContent(201);
+    }
+}
