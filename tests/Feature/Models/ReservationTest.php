4faed90bde51cc81a95150fc7eca808fa2bc2eb2
ryamao <yamao.ryouta@gmail.com>
2024-03-06 20:21:45 +0900

    バグ調査のために、DBに保存された日時データの動作を調査する

--- tests/Feature/Models/ReservationTest.php
+++ tests/Feature/Models/ReservationTest.php
@@ -6,34 +6,42 @@ use App\Models\User;
 use Illuminate\Support\Carbon;
 
 describe('App.Models.Reservation', function () {
-    describe('reserved_at', function () {
-        test('Carbonが返る', function () {
-            $user = User::factory()->create();
-            $shop = Shop::factory()->create();
-            $user->reservedShops()->attach($shop, [
-                'reserved_at' => new Carbon('2022-03-04 18:00:00', 'Asia/Tokyo'),
-                'number_of_guests' => 2,
-            ]);
-            $reservation = Reservation::first();
-            $this->assertInstanceOf(Carbon::class, $reservation->reserved_at);
-        });
+    test('reserved_atがCarbonを返す', function () {
+        $reservedAt = new Carbon('2022-03-04 18:00:00');
+        $user = User::factory()->create();
+        $shop = Shop::factory()->create();
+        $user->reservedShops()->attach($shop, [
+            'reserved_at' => $reservedAt,
+            'number_of_guests' => 2,
+        ]);
+        $reservation = Reservation::first();
+        $this->assertInstanceOf(Carbon::class, $reservation->reserved_at);
+        $this->assertEquals($reservedAt, $reservation->reserved_at);
     });
 
-    describe('user', function () {
-        test('関連するUserが返る', function () {
-            $user = User::factory()->create();
-            $shop = Shop::factory()->create();
-            $reservation = Reservation::factory()->for($user)->for($shop)->create();
-            $this->assertSame($user->id, $reservation->user->id);
-        });
+    test('UTC以外のタイムゾーンを保存した場合', function () {
+        $reservedAt = new Carbon('2022-03-04 18:00:00', 'Asia/Tokyo');
+        $user = User::factory()->create();
+        $shop = Shop::factory()->create();
+        $user->reservedShops()->attach($shop, [
+            'reserved_at' => $reservedAt,
+            'number_of_guests' => 2,
+        ]);
+        $reservation = Reservation::first();
+        $this->assertSame('2022-03-04T18:00:00+00:00', $reservation->reserved_at->toRfc3339String());
     });
 
-    describe('shop', function () {
-        test('関連するShopが返る', function () {
-            $user = User::factory()->create();
-            $shop = Shop::factory()->create();
-            $reservation = Reservation::factory()->for($user)->for($shop)->create();
-            $this->assertSame($shop->id, $reservation->shop->id);
-        });
+    test('userが関連するUserを返す', function () {
+        $user = User::factory()->create();
+        $shop = Shop::factory()->create();
+        $reservation = Reservation::factory()->for($user)->for($shop)->create();
+        $this->assertSame($user->id, $reservation->user->id);
+    });
+
+    test('shopが関連するShopを返す', function () {
+        $user = User::factory()->create();
+        $shop = Shop::factory()->create();
+        $reservation = Reservation::factory()->for($user)->for($shop)->create();
+        $this->assertSame($shop->id, $reservation->shop->id);
     });
 });
