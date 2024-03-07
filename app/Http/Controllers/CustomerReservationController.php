9c5a7c4e6653accbaea73f8a845c4ac361eba9a9
ryamao <yamao.ryouta@gmail.com>
2024-03-07 20:37:27 +0900

    マイページでの予約一覧取得機能のSwaggerスキーマを追加する

--- app/Http/Controllers/CustomerReservationController.php
+++ app/Http/Controllers/CustomerReservationController.php
@@ -4,12 +4,30 @@ namespace App\Http\Controllers;
 
 use App\Models\Reservation;
 use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Gate;
 use OpenApi\Attributes as OA;
 
 class CustomerReservationController extends Controller
 {
+    #[OA\Get(
+        operationId: 'get-customer-reservations',
+        path: '/customers/{customer}/reservations',
+        tags: ['Reservation'],
+        summary: 'マイページでの予約一覧取得機能',
+        description: 'セッション中の顧客が行っている予約の一覧を取得する',
+    )]
+    #[OA\Parameter(ref: '#/components/parameters/customer-id')]
+    #[OA\Response(response: 200, ref: '#/components/responses/get-customer-reservations-200')]
+    #[OA\Response(response: 401, ref: '#/components/responses/unauthorized')]
+    #[OA\Response(response: 403, ref: '#/components/responses/forbidden')]
+    #[OA\Response(response: 404, ref: '#/components/responses/not-found')]
+    public function index(User $customer): JsonResponse
+    {
+        throw new \Exception('未実装');
+    }
+
     #[OA\Delete(
         operationId: 'delete-customer-reservations',
         path: '/customers/{customer}/reservations/{reservation}',
