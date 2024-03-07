76eaced0791576e1df9bf23b7ec01c6fe7296cb6
ryamao <yamao.ryouta@gmail.com>
2024-03-07 20:06:37 +0900

    マイページでのお気に入り一覧取得機能のSwaggerスキーマを追加する

--- app/Http/Controllers/CustomerFavoriteController.php
+++ app/Http/Controllers/CustomerFavoriteController.php
@@ -4,6 +4,26 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use OpenApi\Attributes as OA;
+
 class CustomerFavoriteController extends Controller
 {
+    #[OA\Get(
+        operationId: 'get-customer-favorites',
+        path: '/customers/{customer}/favorites',
+        tags: ['Favorite'],
+        summary: 'マイページでのお気に入り一覧取得機能',
+        description: 'セッション中の顧客がお気に入り登録している飲食店の一覧を取得する',
+    )]
+    #[OA\PathParameter(ref: '#/components/parameters/customer-id')]
+    #[OA\Response(response: 200, ref: '#/components/responses/get-customer-favorites-200')]
+    #[OA\Response(response: 401, ref: '#/components/responses/unauthorized')]
+    #[OA\Response(response: 403, ref: '#/components/responses/forbidden')]
+    #[OA\Response(response: 404, ref: '#/components/responses/not-found')]
+    public function index(User $customer): JsonResponse
+    {
+        throw new \Exception('Not implemented');
+    }
 }
